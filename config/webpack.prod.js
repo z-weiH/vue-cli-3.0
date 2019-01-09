f65cd071466b34349f9dbeac3831ef2d6b4e57ad
zengwei <18279047775@163.com>
2019-01-09 09:57:13 +0800

    完善逻辑

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,15 +1,21 @@
 module.exports = {
+  // 配置项目 路径
+  publicPath : '/',
   configureWebpack : {
+    // 忽略 使用cdn加速
     externals: {
       vue: "Vue",
       vuex: "Vuex",
       "vue-router": "VueRouter",
     },
   },
+  // 修改打包 模板
   pages: {
     index: {
       entry: 'src/main.js',
       template: 'public/index.prod.html',
     },
-  }
+  },
+  // 删除 map文件
+  productionSourceMap : false,
 }
