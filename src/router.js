b3f10145b734be8007fc6bc97d463fba231512c2
zengwei <18279047775@163.com>
2019-03-22 15:00:29 +0800

    router.js 增加公共导入页面方法

--- src/router.js
+++ src/router.js
@@ -1,23 +1,24 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
 
 Vue.use(Router)
+
+let pageView = (name,chunk) => {
+  let aaa = name;
+  return () => import(/*webpackChunkName:  "[request]"*/ `@/views/${name}.vue`)
+}
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/home',
       name: 'home',
-      component: Home
+      component: pageView('Home'),
     },
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: pageView('About'),
     }
   ]
 })
